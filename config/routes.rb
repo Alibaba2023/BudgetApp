702ed7867c8468919ab47db71aaa420e621a6a26
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-11-24 17:02:16 +0430

    Database configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'payments/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'welcome_pages#index'
+
+  devise_for :users
+
+  resources :categories, only: [:index, :new, :create] do
+    resources :payments, only: [:index]
+  end
+
+  resources :payments, only: [:new, :create]
+
 end
